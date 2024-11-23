c8e5390cb193ea416a7a92c38ffad86cb73b6d7b
Adithyenkandasamy <adithyen1@gmail.com>
2024-11-23 20:51:07 +0530

    next que

--- 1431. Kids With the Greatest Number of Candies/ANS.py
+++ 1431. Kids With the Greatest Number of Candies/ANS.py
@@ -11,3 +11,7 @@ class Solution:
         print(result)
         return result       
             
+# effecient solution
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [candy + extraCandies >= max(candies) for candy in candies] 
