c6729fbdb872fb2aa91b685fb9858545135e506f
Adithyenkandasamy <adithyen1@gmail.com>
2025-04-02 11:01:43 +0530

    ntg

--- 66.Plusone/ANS.py
+++ 66.Plusone/ANS.py
@@ -1,10 +1,11 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+    def plusOne(self, digits):
         for i in range(len(digits) - 1, -1, -1):
             if digits[i] < 9: 
                 digits[i] += 1
                 return digits
             digits[i] = 0  
 
-        return [1] + digits
-    
+        return [1] + digits 
+p=Solution()
+print(p.plusOne([1,2,3]))  # Output: [1, 2, 4]  
